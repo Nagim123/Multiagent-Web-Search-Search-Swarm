94584ef1730483564c7bd269f057b09e727fd898
Nagim123 <fourcolorsgame@gmail.com>
2024-10-05 17:45:10 +0300

    prepare other llm for developing stage

--- search_swarm_core/attribute_chooser_llm.py
+++ search_swarm_core/attribute_chooser_llm.py
@@ -1,10 +1,17 @@
 from typing import List
+from pydantic import BaseModel
+from openai import OpenAI
+
+from config_reader import ConfigReader
 from search_swarm_core.search_llm import Product
 
 class AttributeChooserLLM:
 
     def __init__(self) -> None:
-        pass
+        self.selecting_prompt = """
+        Place prompt here
+        """
+        self.client = OpenAI(api_key=ConfigReader.instance.get("open_ai_api_key"))
 
-    def choose_atrributes(self, product: Product, button_attributes: List[str]) -> List[str]:
+    def choose_atrributes(self, product: Product, button_attributes: List[str], requirements: str) -> List[str]:
         pass
--- search_swarm_core/critique_llm.py
+++ search_swarm_core/critique_llm.py
@@ -1,10 +1,18 @@
 from typing import List
+from pydantic import BaseModel
+from openai import OpenAI
+
+from config_reader import ConfigReader
 from search_swarm_core.search_llm import Product
+
 
 class CritiqueLLM:
 
     def __init__(self) -> None:
-        pass
+        self.critique_prompt = """
+        Place prompt here
+        """
+        self.client = OpenAI(api_key=ConfigReader.instance.get("open_ai_api_key"))
 
     def get_best_product(products: List[Product]) -> Product:
         pass
